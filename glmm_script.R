94f28925b8d6bf17470c7c3e0b27439c41045b41
Crowley <lcrowley1@apsu.edu>
2023-10-16 11:17:23 -0500

    Initial activity

---
+++ glmm_script.R
@@ -0,0 +1,17 @@
+# Mixed Models 
+# Leah N. Crowley - Fall 2023 
+
+# Call requried packages to workspace: 
+library(ggplot2)
+library(dplyr)
+library(arm)
+library(MASS)
+library(ggfortify)
+library(nlme)
+library(lme4)
+library(lmerTest)
+library(emmeans)
+library(ggtext)
+library(patchwork)
+
+# Read in data you are using for this assignment: 
